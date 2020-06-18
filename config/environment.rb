1da52306df9b53981b53b32b0ad818f1e0eb670f
AMHOL <andyholland1991@aol.com>
2020-06-18 23:27:59 +0800

    Add restart: always to docker-compose.yml

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,6 @@ post '/replays/metadata' do
   Openra::API::Endpoints::Replays::Metadata.new.(self, params)
 end
 
-
 post '/replays/data' do
   Openra::API::Endpoints::Replays::Data.new.(self, params)
 end
